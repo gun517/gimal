10cb50b05b5939b75c67ddf86db9a30a44dceccc
khun <rigel517@gmail.com>
2016-06-08 14:26:34 +0900

    MEDIA 설정

--- gimal/urls.py
+++ gimal/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from shop import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
